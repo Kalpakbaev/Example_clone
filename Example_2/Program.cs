f4f95aef733830d7c255f3c8e251eb8c4ec50808
Kalpakbaev <kozha_1992@mail.ru>
2022-12-06 20:14:54 +0500

    Example_2

---
+++ Example_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// : Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int UniCode(){
+    Console.WriteLine("Введите трехзначное число");
+    int UserNumber = Convert.ToInt32(Console.ReadLine());
+    if(UserNumber < 100){
+        Console.WriteLine("Третьей цифры нет");
+        return UserNumber;
+    }else{ 
+        int three = UserNumber % 10;
+        Console.WriteLine("Третья цифра введеного числа ");
+        return three;
+    }
+}
+int res = UniCode();
+Console.WriteLine(res);
